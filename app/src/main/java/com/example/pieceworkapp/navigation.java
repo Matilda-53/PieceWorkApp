d84a00267ff365bc27fbb93d45852515dd5dbcb6
Matilda Isaac <bed-com-40-18@unima.ac.mw>
2022-10-28 17:58:00 +0200

    Navigation bar

---
+++ app/src/main/java/com/example/pieceworkapp/navigation.java
@@ -0,0 +1,13 @@
+package com.example.pieceworkapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
