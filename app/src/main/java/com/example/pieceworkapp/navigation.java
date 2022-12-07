8c28bc5e63640f3c02ee03463652b7e25fd63665
Matilda Isaac <bed-com-40-18@unima.ac.mw>
2022-12-07 17:18:54 +0200

    Navigation Bar

--- app/src/main/java/com/example/pieceworkapp/navigation.java
+++ app/src/main/java/com/example/pieceworkapp/navigation.java
@@ -1,21 +1,17 @@
 package com.example.pieceworkapp;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -39,6 +35,7 @@ public class navigation extends AppCompatActivity implements NavigationView.OnNa
         toggle.syncState();
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -73,79 +70,5 @@ public class navigation extends AppCompatActivity implements NavigationView.OnNa
     }
 
 
-    //    TextView list;
-//    TextView cate;
-//    TextView in_box;
-//    TextView home;
-//
-//
-//
-//    private  DrawerLayout drawerLayout;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//         super.onCreate(savedInstanceState);
-//         setContentView(R.layout.activity_navigation);
-//         drawerLayout =findViewById(R.id.nav_view);
-//         Toolbar toolbar = findViewById(R.id.toolbar);
-//         NavigationView navigationView = findViewById(R.id.navigation_view);
-//         list = (TextView) findViewById(R.id.list);
-//         cate = (TextView) findViewById(R.id.cate);
-//         in_box = (TextView) findViewById(R.id.in_box);
-//         home = (TextView) findViewById(R.id.home);
-//
-//
-//
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-//        drawerLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        //create list
-//        list.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(navigation.this, createList.class));
-//
-//            }
-//        });
-//
-//        cate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(navigation.this, category.class));
-//
-//            }
-//        });
-//           //inbox
-//        in_box.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(navigation.this, Inbox.class));
-//
-//            }
-//        });
-//        //home
-//        home.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(navigation.this, Home.class));
-//
-//            }
-//        });
-//    }
-//    @Override
-//    public void onBackPressed(){
-//        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-//            drawerLayout.closeDrawer(GravityCompat.START);
-//        }
-//          else {
-//              super.onBackPressed();
-//        }
-//    }
-//    private void sendUserToNextActivity() {
-//        Intent intent = new Intent(navigation.this, Home.class);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-//        startActivity(intent);
-//
-//    }
 
 }
